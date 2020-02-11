dfa21083e6afe4a3e879dd4a97644c2b0a01efbd
Brendon Fatland <b.fatland@gmail.com>
2020-02-10 18:46:15 -0600

    Add little style change. Add database files.

--- POneTest2/Models/studentContext.cs
+++ POneTest2/Models/studentContext.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
 namespace POneTest2.Models
 {
-    public class studentContext
+    public class studentContext : DbContext
     {
+        public DbSet<DentistData> Dentist { get; set; }
     }
 }
